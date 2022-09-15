61104359ee1d6efbe519cfb163183227b99b71c3
Panos Bozas <fbozas@hotmail.com>
2022-09-15 20:15:19 +0300

    Create DbContext and set the connection string

---
+++ CleanArchitecture.DataAccess/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.DataAccess.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
