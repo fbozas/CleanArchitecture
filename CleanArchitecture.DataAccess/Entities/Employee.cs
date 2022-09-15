5e816aa4c1d3e2c149d29a1bf5546ea45c58a943
Panos Bozas <fbozas@hotmail.com>
2022-09-15 20:07:12 +0300

    Added the entities (models)

---
+++ CleanArchitecture.DataAccess/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.DataAccess.Entities
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Depid { get; set; }
+        public double Salary { get; set; }
+        public DateTime Hiredate { get; set; }
+        public Department Department { get; set; }
+    }
+}
