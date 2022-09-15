ced86cfdc023e02c435bf7893c500605d84eb180
Panos Bozas <fbozas@hotmail.com>
2022-09-15 20:37:12 +0300

    Added migrations

---
+++ CleanArchitecture.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CleanArchitecture.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CleanArchitecture.DataAccess.Persistence.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CleanArchitecture.DataAccess.Persistence.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
