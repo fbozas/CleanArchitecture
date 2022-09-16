47273debf9f95b856128e58da5896a6d4fd58a45
Panos Bozas <fbozas@hotmail.com>
2022-09-16 19:07:01 +0300

    Dependency Injection: Added all the configurations

---
+++ CleanArchitecture.MVC/App_Start/ContainerConfig.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using CleanArchitecture.DataAccess.Interfaces;
+using CleanArchitecture.DataAccess.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CleanArchitecture.MVC.App_Start
+{
+    public class ContainerConfig
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Register DbContext
+            builder.RegisterType<ApplicationDbContext>().InstancePerRequest();
+
+            // Register UnitOfWork
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        public static void RegisterContainerApi()
+        {
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register DbContext
+            builder.RegisterType<ApplicationDbContext>().InstancePerRequest();
+
+            //Register UnitOfWork
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
--- CleanArchitecture.MVC/Global.asax.cs
+++ CleanArchitecture.MVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecture.MVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,8 @@ namespace CleanArchitecture.MVC
     {
         protected void Application_Start()
         {
+            ContainerConfig.RegisterContainer();
+            ContainerConfig.RegisterContainerApi();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
