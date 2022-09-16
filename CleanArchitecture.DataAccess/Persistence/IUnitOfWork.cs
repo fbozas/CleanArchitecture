80c3bb762fad93435882836f05bb28673545de16
Panos Bozas <fbozas@hotmail.com>
2022-09-16 18:36:00 +0300

    Created UnitOfWork and IUnitOfWork

---
+++ CleanArchitecture.DataAccess/Persistence/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.DataAccess.Interfaces;
+using System;
+
+namespace CleanArchitecture.DataAccess.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDepartmentRepository Departments { get; }
+        IEmployeeRepository Employees { get; }
+
+        void Complete();
+    }
+}
