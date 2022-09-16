62da2e4100ec85c308f98c3c2142ddea693e8a2c
Panos Bozas <fbozas@hotmail.com>
2022-09-16 19:12:42 +0300

    Added Dtos

---
+++ CleanArchitecture.MVC/Dtos/EmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CleanArchitecture.MVC.Dtos
+{
+    public class EmployeeDto
+    {
+        public int ID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Depid { get; set; }
+        public double Salary { get; set; }
+        public DateTime Hiredate { get; set; }
+    }
+}
