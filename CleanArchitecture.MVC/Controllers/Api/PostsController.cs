0ab2a4b724cd4f3d46cadec2dd08d958711cda3e
Panos Bozas <fbozas@hotmail.com>
2022-09-21 18:54:01 +0300

    Added WebApi.Cors 5.0.0 solved the issue

--- CleanArchitecture.MVC/Controllers/Api/PostsController.cs
+++ CleanArchitecture.MVC/Controllers/Api/PostsController.cs
@@ -8,9 +8,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace CleanArchitecture.MVC.Controllers.Api
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
     public class PostsController : ApiController
     {
         private readonly IUnitOfWork _unitOfWork;
